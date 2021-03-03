023e15dee2db34b32e0637d5e9a84de237c722c0
AndreyAmelchenia <pmad1986@gmail.com>
2021-03-03 12:49:16 +0300

    init: start book shop

---
+++ .prettierrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  // extends: ["prettier"],
+  // plugins: ["prettier"],
+  // rules: {
+  //   "prettier/prettier": ["error", {"singleQuote": true, "parser": "flow"}]
+  // },
+  trailingComma: "all",
+  semi: true,
+  singleQuote: false,
+  bracketSpacing: true,
+  printWidth: 100,
+  // quoteProps: "as-needed",
+  arrowParens: "always",
+  overrides: [
+    {
+        files: "*.component.html",
+        options: {
+            parser: "angular"
+        }
+    },
+    {
+        files: "*.html",
+        options: {
+            parser: "html"
+        }
+    }
+]
+};
